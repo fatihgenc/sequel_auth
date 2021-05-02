2ed33d4e3aa24e15824a4c950ff005203b5ee0e8
Fatih GENÇ <fatih.genc@elanbilisim.com.tr>
2021-05-02 22:26:48 +0300

    spec comment removed

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,23 +23,7 @@ RSpec.configure do |c|
     class User < Sequel::Model
       plugin :sequel_auth
     end
-    
-   # class BcryptUser < Sequel::Model
-   #   plugin :sequel_auth, provider: :bcrypt
-   # end
-   # 
-   # class ScryptUser < Sequel::Model
-   #   plugin :sequel_auth, provider: :scrypt
-   # end
-   # 
-   # class CryptUser < Sequel::Model
-   #   plugin :sequel_auth, provider: :crypt
-   # end
-   # 
-   # class DifferentDigestUser < Sequel::Model
-   #   plugin :sequel_auth, digest_column: :password_hash
-   # end
-    
+   
   end
   
 end
